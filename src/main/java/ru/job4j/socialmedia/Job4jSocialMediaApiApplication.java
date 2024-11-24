26b8063013d048b20eec939a683a57410be1f813
GANZO9055 <kosta.maslihov@gmail.com>
2024-11-24 22:43:28 +0300

    Доменная модель [#505091]

--- src/main/java/ru/job4j/socialmedia/Job4jSocialMediaApiApplication.java
+++ src/main/java/ru/job4j/socialmedia/Job4jSocialMediaApiApplication.java
@@ -2,17 +2,9 @@ package ru.job4j.socialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class Job4jSocialMediaApiApplication {
-
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
