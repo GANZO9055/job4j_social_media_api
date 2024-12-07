866be2ba1db038c160449cf9c6387893b20a1f3b
GANZO9055 <kosta.maslihov@gmail.com>
2024-12-07 21:56:39 +0300

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/socialmedia/repository/tape/TapeRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/tape/TapeRepository.java
@@ -1,7 +1,22 @@
 package ru.job4j.socialmedia.repository.tape;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.socialmedia.model.Tape;
+import ru.job4j.socialmedia.model.User;
 
-public interface TapeRepository extends CrudRepository<Tape, Integer> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TapeRepository extends JpaRepository<Tape, Integer> {
+
+    List<Tape> findByUser(User user);
+
+    List<Tape> findByCreatedPostGreaterThanEqualAndCreatedPostLessThanEqual(
+            LocalDateTime startTime,
+            LocalDateTime endTime
+    );
+
+    Page<Tape> findByOrderByCreatedPostDesc(Pageable pageable);
 }
