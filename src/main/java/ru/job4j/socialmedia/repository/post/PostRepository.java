79230f4748f568959c357399c31d0109999f5083
GANZO9055 <kosta.maslihov@gmail.com>
2024-12-08 20:08:35 +0300

    HQL, JPQL, Native SQL [#505064]

--- src/main/java/ru/job4j/socialmedia/repository/post/PostRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/post/PostRepository.java
@@ -1,7 +1,40 @@
 package ru.job4j.socialmedia.repository.post;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmedia.model.Post;
 
 public interface PostRepository extends CrudRepository<Post, Integer> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            UPDATE posts
+            SET title = :title1, description = :description2
+            WHERE id = :id3
+            """
+    )
+    int updateTitleAndDescriptionById(
+            @Param("title1") String title,
+            @Param("description2") String description,
+            @Param("id3") Integer id
+    );
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            DELETE FROM posts
+            WHERE id = :id
+            """
+    )
+    int deletePostById(@Param("id") Integer id);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            value = """
+            DELETE FROM files
+            WHERE post_id = :id
+            """, nativeQuery = true
+    )
+    int deleteFileById(@Param("id") Integer id);
 }
--- src/main/java/ru/job4j/socialmedia/repository/user/UserRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/user/UserRepository.java
@@ -1,7 +1,49 @@
 package ru.job4j.socialmedia.repository.user;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import ru.job4j.socialmedia.model.Post;
 import ru.job4j.socialmedia.model.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query("""
+            SELECT * FROM users
+            WHERE email = :email1 AND password = :password2
+            """
+    )
+    Optional<User> findByEmailAndPassword(
+            @Param("email1") String email,
+            @Param("password2") String password
+    );
+
+    @Query(value = """
+            SELECT * FROM subscriptions
+            WHERE user_target_id = :id
+            """, nativeQuery = true
+    )
+    List<User> findByIdAllSubscriptions(@Param("id") Integer id);
+
+    @Query(value = """
+            SELECT * FROM friends
+            WHERE requester_id = :id
+            """, nativeQuery = true
+    )
+    List<User> findByIdAllFriends(@Param("id") Integer id);
+
+    @Query(value = """
+            SELECT p.user_id, p.title, p.description
+            FROM posts AS p
+            JOIN tapes AS t ON p.id = t.post_id
+            JOIN subscriptions AS s ON s.user_subscriber_id = p.user_id
+            WHERE s.user_target_id = :id
+            ORDER BY t.created_post DESC
+            """, nativeQuery = true
+    )
+    List<Post> findByIdAllPostsSubscriptions(@Param("id") Integer id, Pageable pageable);
 }
